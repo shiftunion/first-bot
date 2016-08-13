f24b551d8b5c6bc2a5619e8a243d9436248444b2
ShiftUnion <nick.cahill@gmail.com>
2016-08-13 16:16:01 +1200

    Added the call back and validation token

--- index.js
+++ index.js
@@ -28,3 +28,39 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAALI023ztRYBAFkiqGCcjrkB04OZALVagcZB1zOXT8h7o5SwEpPFzKrMtei9YpWlCxRomvC3Jz0F5yPYBlSwYimdQA9EvClrOvTaZCJhqYRpZByMBiI1wQsL1dV5slQZC4JudI82uCZCD8XSZA74OXoZCfZAwe7O1l8rDciQMFxAL0AZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
