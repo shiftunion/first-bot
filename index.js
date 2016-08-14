311602084ec1fb2359caf678d16f6998c6dc3e03
ShiftUnion <nick.cahill@gmail.com>
2016-08-14 21:10:02 +1200

    Tweaks for deployment

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/webhook/', function (req, res) {
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            sendTextMessage(sender, "Postback received-" + sender + " " + text.substring(0, 200), token)
             continue
         }
     }
