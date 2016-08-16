0d7b93fb4c0e8f4d0d3096034824241d6a90f5d2
Nick Cahill <shiftunion@users.noreply.github.com>
2016-08-16 22:37:27 +1200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 var request = require('request');
 
+function workathon() {
+
 var myJSONObject = {
     "recipient": {"id": "1255533624481471"},
     "message": {"text": "hello, dude! do you want to get fat @ " + getFormattedDate()}
 };
-
-function workathon() {
 
     request({
         url: "https://graph.facebook.com/v2.6/me/messages?access_token=EAALI023ztRYBAFkiqGCcjrkB04OZALVagcZB1zOXT8h7o5SwEpPFzKrMtei9YpWlCxRomvC3Jz0F5yPYBlSwYimdQA9EvClrOvTaZCJhqYRpZByMBiI1wQsL1dV5slQZC4JudI82uCZCD8XSZA74OXoZCfZAwe7O1l8rDciQMFxAL0AZDZD",
