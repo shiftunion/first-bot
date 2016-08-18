4958b97465f4582cb0902d69f05d9f2bdfd449f5
ShiftUnion <nick.cahill@gmail.com>
2016-08-18 20:50:10 +1200

    2 hour  timer No trigger on commit

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ var timeZone = "Pacific/Auckland";
 
 
 var CronJob = require('cron').CronJob;
-new CronJob('0 */40 * * * *', function() {
+new CronJob('00 0 */2 * * *', function() {
     worker.start();
 }, function(){ console.log('finished cron')}
 
