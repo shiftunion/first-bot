ff2adf3cf6558d57661ee72ca92ba84cdccfd0dc
ShiftUnion <nick.cahill@gmail.com>
2016-08-22 19:57:17 +1200

    5 hour trigger

--- clock.js
+++ clock.js
@@ -8,7 +8,7 @@ var timeZone = "Pacific/Auckland";
 
 
 var CronJob = require('cron').CronJob;
-new CronJob('00 0 */5 * * *', function() {
+new CronJob('00 0 */8 * * *', function() {
     worker.start();
 }, function(){ console.log('finished cron')}
 
